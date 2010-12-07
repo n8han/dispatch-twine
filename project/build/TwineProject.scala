c23c686ad5d75fb0a396cdf8e2dd4a5de3a39f0c
Nathan Hamblen <nathan@technically.us>
2010-12-07 01:18:10 -0500

    0.7.8

--- project/build/TwineProject.scala
+++ project/build/TwineProject.scala
@@ -3,5 +3,5 @@ import Process._
 
 class TwineProject(info: ProjectInfo) extends DefaultProject(info) {
   val configgy = "net.lag" % "configgy" % "2.0.0" intransitive()
-  val dispatch = "net.databinder" %% "dispatch-twitter" % "0.7.8-SNAPSHOT"
+  val dispatch = "net.databinder" %% "dispatch-twitter" % "0.7.8"
 }
