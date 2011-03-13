d70df5bef0575a724d6cfa3b49d7f70b88a7a228
Nathan Hamblen <nathan@technically.us>
2011-03-13 19:20:07 -0400

    beta4

--- project/build/TwineProject.scala
+++ project/build/TwineProject.scala
@@ -3,7 +3,7 @@ import Process._
 
 class TwineProject(info: ProjectInfo) extends DefaultProject(info) {
   val configgy = "net.lag" % "configgy" % "2.0.0" intransitive()
-  val dvers = "0.8.0.Beta3"
+  val dvers = "0.8.0.Beta4"
   val twitter = "net.databinder" %% "dispatch-twitter" % dvers
   val http = "net.databinder" %% "dispatch-nio" % dvers
 }
