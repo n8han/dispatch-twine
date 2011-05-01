72ec038efd41e78a09bb888f92e319be46576d19
Nathan Hamblen <nathan@technically.us>
2011-05-01 18:47:10 -0400

    add http dep for now

--- project/build/TwineProject.scala
+++ project/build/TwineProject.scala
@@ -6,4 +6,7 @@ class TwineProject(info: ProjectInfo) extends DefaultProject(info) {
   val dvers = "0.8.0"
   val twitter = "net.databinder" %% "dispatch-twitter" % dvers
   val nio = "net.databinder" %% "dispatch-nio" % dvers
+  /* Twine doesn't need the below dependency, but it simplifies
+   * the Dispatch tuturials to keep it here for now. */
+  val http = "net.databinder" %% "dispatch-http" % dvers
 }
