6b52a255602b4153c031782b076993f0835f5a50
Nathan Hamblen <nathan@technically.us>
2011-05-09 07:58:32 -0400

    version

--- project/build/TwineProject.scala
+++ project/build/TwineProject.scala
@@ -3,7 +3,7 @@ import Process._
 
 class TwineProject(info: ProjectInfo) extends DefaultProject(info) {
   val configgy = "net.lag" % "configgy" % "2.0.0" intransitive()
-  val dvers = "0.8.0"
+  val dvers = "0.8.1"
   val twitter = "net.databinder" %% "dispatch-twitter" % dvers
   val nio = "net.databinder" %% "dispatch-nio" % dvers
   /* Twine doesn't need the below dependency, but it simplifies
