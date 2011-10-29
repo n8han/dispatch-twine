70124567c75e675da23a39494f32be5ffd0964dd
Nathan Hamblen <nathan@technically.us>
2011-10-29 18:47:08 -0400

    trans dependencies seem to work fine

--- project/build.scala
+++ project/build.scala
@@ -2,9 +2,7 @@ import sbt._
 object MyApp extends Build
 {
   lazy val root =
-    Project("", file(".")) dependsOn(dispatchLiftJson,dispatchTwitter)
+    Project("", file(".")) dependsOn(dispatchTwitter)
   lazy val dispatchTwitter =
     uri("git://github.com/n8han/dispatch-twitter#0.1.2")
-  lazy val dispatchLiftJson =
-    uri("git://github.com/dispatch/dispatch-lift-json#0.1.0")
 }
